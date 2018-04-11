d8f0dcb010ee4b82945ea6507b7623f0eeef4229
Stefano Pacifici <stefano.pacifici@gmail.com>
2018-04-11 13:01:58 +0200

    AB2-26: basic branding for Cliqz Browser and Cliqz Browser Alpha (#18)

--- mozilla-release/mobile/android/branding/cliqz-alpha/configure.sh
+++ mozilla-release/mobile/android/branding/cliqz-alpha/configure.sh
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 ANDROID_PACKAGE_NAME=com.cliqz.browser.alpha
-MOZ_APP_DISPLAYNAME="Cliqz Browser"
+MOZ_APP_DISPLAYNAME="Cliqz Browser Alpha"
 MOZ_UPDATER=
 MOZ_ANDROID_ANR_REPORTER=
 MOZ_ANDROID_GCM_SENDERID=965234145045
