2867889869433b498f15004e74587bc254389b51
joseph-cliqz <joseph@cliqz.com>
2018-05-28 16:11:57 +0200

    AB2-68 Add parallel building of APKs to CI (#68)
    * Parallel Builds Initial Commit
    
    * Add ghostery and search xpi files and configurations
    
    * Requested Changes after Review

---
+++ mozilla-release/mobile/android/branding/ghostery-alpha/configure.sh
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+ANDROID_PACKAGE_NAME=com.ghostery.android.alpha
+MOZ_APP_DISPLAYNAME="Ghostery Alpha"
+MOZ_UPDATER=
+MOZ_ANDROID_ANR_REPORTER=
+MOZ_ANDROID_GCM_SENDERID=965234145045
+MOZ_MMA_GCM_SENDERID=242693410970
