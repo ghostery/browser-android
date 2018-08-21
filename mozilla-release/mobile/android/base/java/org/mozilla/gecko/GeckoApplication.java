6536b0235e6be1e659d2cc35bb372e4da07cc2ea
Lucian Corlaciu <lucian@cliqz.com>
2018-08-21 10:54:02 +0200

    Remove MultiDex

--- mozilla-release/mobile/android/base/java/org/mozilla/gecko/GeckoApplication.java
+++ mozilla-release/mobile/android/base/java/org/mozilla/gecko/GeckoApplication.java
@@ -22,8 +22,6 @@ import android.os.SystemClock;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
@@ -68,7 +66,7 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.UUID;
 
-public class GeckoApplication extends MultiDexApplication
+public class GeckoApplication extends Application
                               implements HapticFeedbackDelegate {
     private static final String LOG_TAG = "GeckoApplication";
     private static final String MEDIA_DECODING_PROCESS_CRASH = "MEDIA_DECODING_PROCESS_CRASH";
@@ -88,14 +86,6 @@ public class GeckoApplication extends MultiDexApplication
     public GeckoApplication() {
         super();
     }
-
-    /* Cliqz start */
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-    }
-    /* Cliqz end */
 
     public static RefWatcher getRefWatcher(Context context) {
         GeckoApplication app = (GeckoApplication) context.getApplicationContext();
