ec0b9b20a88759c3f5e1435bbf1d877c74a41bb0
ravjit-cliqz <ravjit@cliqz.com>
2018-10-21 11:50:31 +0200

    AB2-569-2 increase the padding a bit more for text in cc (#442)

--- mozilla-release/mobile/android/base/java/org/mozilla/gecko/controlcenter/OverviewFragment.java
+++ mozilla-release/mobile/android/base/java/org/mozilla/gecko/controlcenter/OverviewFragment.java
@@ -234,7 +234,7 @@ public class OverviewFragment extends ControlCenterFragment implements View.OnCl
                 Integer.toString(totalTrackers).length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         mPieChart.setCenterText(centerTextSpan);
         mPieChart.setCenterTextSize(18);
-        mPieChart.setCenterTextRadiusPercent(95);
+        mPieChart.setCenterTextRadiusPercent(90);
         mPieChart.setCenterTextColor(ContextCompat.getColor(getContext(), R.color.cc_text_color));
         mPieChart.setHoleRadius(80);
         mPieChart.setDescription(null);
