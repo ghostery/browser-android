d331e7ee84f944a19683c42df3cf57dc1dfe2b90
Lucian Corlaciu <lucian@cliqz.com>
2019-01-09 16:50:04 +0100

    Limit UA override to google SERP only (#548)

--- mozilla-release/mobile/android/app/mobile.js
+++ mozilla-release/mobile/android/app/mobile.js
@@ -919,8 +919,8 @@ pref("extensions.systemAddon.update.url", "");
 // override the UA on Google SERP pages due to https://bugzilla.mozilla.org/show_bug.cgi?id=975444
 pref("general.useragent.override.encrypted.google.com", "Mozilla/5.0 (Linux; Android 8.0.0; rv:61.0) FxQuantum/61.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.0.0 Mobile Safari/537.36");
 pref("general.useragent.override.encrypted.google.de", "Mozilla/5.0 (Linux; Android 8.0.0; rv:61.0) FxQuantum/61.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.0.0 Mobile Safari/537.36");
-pref("general.useragent.override.google.com", "Mozilla/5.0 (Linux; Android 8.0.0; rv:61.0) FxQuantum/61.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.0.0 Mobile Safari/537.36");
-pref("general.useragent.override.google.de", "Mozilla/5.0 (Linux; Android 8.0.0; rv:61.0) FxQuantum/61.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.0.0 Mobile Safari/537.36");
+pref("general.useragent.override.www.google.com", "Mozilla/5.0 (Linux; Android 8.0.0; rv:61.0) FxQuantum/61.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.0.0 Mobile Safari/537.36");
+pref("general.useragent.override.www.google.de", "Mozilla/5.0 (Linux; Android 8.0.0; rv:61.0) FxQuantum/61.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.0.0 Mobile Safari/537.36");
 
 /* Cliqz start */
 pref("media.autoplay.enabled", false);
