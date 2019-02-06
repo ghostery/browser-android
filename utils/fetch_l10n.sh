47c1b625af0b1cba0dacb2a26befbd716378e02e
Stefano Pacifici <stefano.pacifici@gmail.com>
2019-02-06 18:59:04 +0100

    AB-773-2 Moving es-ES to be simply es

--- utils/fetch_l10n.sh
+++ utils/fetch_l10n.sh
@@ -5,16 +5,30 @@ readonly ABSDIR=`readlink -f "${RELDIR}"`
 readonly L10NDIR=`readlink -f "${RELDIR}/../l10n"`
 readonly TMPDIR=`readlink -f "${RELDIR}/../.l10n"`
 readonly REPDIR=`readlink -f "${RELDIR}/../.l10n-rep"`
-readonly LANGS=`cd "${L10NDIR}"; ls -1d */`
 readonly LOCALE_JSON=`readlink -f "${ABSDIR}/../mozilla-release/mobile/locales/l10n-changesets.json"`
+declare -rA LANGS=( \
+    ["de"]="de" \
+    ["es"]="es-ES" \
+    ["fr"]="fr" \
+    ["hu"]="hu" \
+    ["it"]="it" \
+    ["ja"]="ja" \
+    ["ko"]="ko" \
+    ["nl"]="nl" \
+    ["pl"]="pl" \
+    ["pt-BR"]="pt-BR" \
+    ["ru"]="ru" \
+    ["zh-CN"]="zh-CN" \
+    ["zh-TW"]="zh-TW" \
+)
 
 function finish {
     rm -rf "${TMPDIR}" "${REPDIR}"
 }
 
 function fetch {
-    revision=`cat "${LOCALE_JSON}"|jq ".\"${1}\".revision"`
-    hg clone "https://hg.mozilla.org/l10n-central/$1" "$1"
+    revision=`cat "${LOCALE_JSON}"|jq ".\"${2}\".revision"`
+    hg clone "https://hg.mozilla.org/l10n-central/$2" "$1"
     cd $1
     hg update "${revision}"
     rm -rf .hg .hgtags .hgignore
@@ -25,10 +39,21 @@ trap finish EXIT
 # Making tmp dir
 mkdir -p "${TMPDIR}"
 
-for lang in ${LANGS}
+for lang in ${!LANGS[@]}
 do
-    (cd "${TMPDIR}"; fetch "${lang%/}")
+    (cd "${TMPDIR}"; fetch "${lang}" "${LANGS[$lang]}")
 done
 
 mv "${L10NDIR}" "${REPDIR}"
 mv "${TMPDIR}" "${L10NDIR}"
+
+# Make sure to revert the Cliqz strings
+(
+    cd "${ABSDIR}/../"
+    files="`git diff --name-only --diff-filter=D|grep cliqz`"
+    for f in ${files}
+    do
+        git checkout -- "${f}"
+    done
+)
+
