5f38290a09271f3a9db5f91ea024c2f02655470a
giota-cliqz <panagiota@cliqz.com>
2019-02-18 17:37:00 +0100

    AB2-846 History

--- mozilla-release/mobile/android/base/java/com/cliqz/react/BrowserActions.java
+++ mozilla-release/mobile/android/base/java/com/cliqz/react/BrowserActions.java
@@ -1,11 +1,20 @@
 package com.cliqz.react;
 
+import android.content.Context;
+import android.database.Cursor;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
 
 import org.mozilla.gecko.EventDispatcher;
+import org.mozilla.gecko.GeckoThread;
+import org.mozilla.gecko.db.BrowserDB;
 import org.mozilla.gecko.util.GeckoBundle;
 
 /**
@@ -43,6 +52,31 @@ public class BrowserActions extends ReactContextBaseJavaModule {
         bundle.putString("query", query);
         bundle.putStringArray("suggestions", suggestionsArray );
         EventDispatcher.getInstance().dispatch("Search:QuerySuggestions", bundle);
+    }
+
+    @ReactMethod
+    public void searchHistory(String query, Promise promise) {
+        final int limit = 5;
+        final BrowserDB sdb = BrowserDB.from(GeckoThread.getActiveProfile());
+        final Context context = getCurrentActivity().getApplicationContext();
+        final Cursor cursor = sdb.getRankedHistoryForQuery(context.getContentResolver(), query, limit);
+
+        final WritableArray wArray = Arguments.createArray();
+
+        if (!cursor.moveToFirst()) {
+            return;
+        }
+
+        do {
+            final String title = cursor.getString(cursor.getColumnIndex("title"));
+            final String url = cursor.getString(cursor.getColumnIndex("url"));
+            final WritableMap outData = Arguments.createMap();
+            outData.putString("title", title);
+            outData.putString("url", url);
+            wArray.pushMap(outData);
+        } while (cursor.moveToNext());
+
+        promise.resolve(wArray);
     }
 
 }
