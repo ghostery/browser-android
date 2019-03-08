4a013d3a4c01ae66bf0f784caf4d6f98288d00cc
Krzysztof Jan Modras <krzysztof.modras@gmail.com>
2019-03-08 12:07:10 +0100

    migration manager rely on cliqz version code, not gecko one (#707)

--- mozilla-release/mobile/android/base/java/com/cliqz/react/MigrationManager.java
+++ mozilla-release/mobile/android/base/java/com/cliqz/react/MigrationManager.java
@@ -2,20 +2,22 @@ package com.cliqz.react;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import com.cliqz.ABManager;
 import com.cliqz.react.modules.PrefsModule;
 
+import org.mozilla.gecko.GeckoApp;
 import org.mozilla.gecko.GeckoSharedPrefs;
 import org.mozilla.gecko.PrefsHelper;
 import org.mozilla.gecko.preferences.GeckoPreferences;
-
-import static org.mozilla.gecko.GeckoApp.PREFS_VERSION_CODE;
 
 /**
  * Copyright © Cliqz 2019
  */
 public class MigrationManager {
+    private static final String PREFS_VERSION_CODE = "cliqzVersionCode";
     private static final int GHOSTERY_VERSION_2_2_1 = 12255;
     private static final int GHOSTERY_VERSION_2_3 = 12555;
 
@@ -25,35 +27,10 @@ public class MigrationManager {
         return ourInstance;
     }
 
-    /**
-     * Let Search handle search language
-     */
-    private void migrateBackendCountryLanguage() {
-        PrefsHelper.getPref(GeckoPreferences.PREFS_SEARCH_REGIONAL, new PrefsHelper.PrefHandlerBase() {
-            @Override
-            public void prefValue(String prefName, String countryCode) {
-            if (!(countryCode instanceof String) || countryCode.equals("")) {
-                return;
-            }
-            PrefsHelper.setPref(GeckoPreferences.PREFS_SEARCH_REGIONAL, "");
-            SearchBackground.setBackendCountry(countryCode);
-            }
-        });
-    }
-
-    private void migrateQuerySuggestionsPref() {
-        PrefsHelper.getPref(GeckoPreferences.PREFS_SEARCH_QUERY_SUGGESTIONS, new PrefsHelper.PrefHandlerBase() {
-            @Override
-            public void prefValue(String prefName, boolean enabled) {
-                PrefsHelper.setPref(GeckoPreferences.PREFS_SEARCH_QUERY_SUGGESTIONS, "");
-                PrefsHelper.setPref(PrefsModule.PREFS_SEARCH_QUERY_SUGGESTIONS, enabled);
-            }
-        });
-    }
-
     public void migrate(Context context) {
-        final SharedPreferences prefs = GeckoSharedPrefs.forProfile(context);;
-        final int prefsVersionCode = prefs.getInt(PREFS_VERSION_CODE, 0);
+        final SharedPreferences prefs = GeckoSharedPrefs.forProfile(context);
+        final int version = getVersionCode(context);
+        final int prefsVersionCode = getPreviousVersionCode(prefs);
 
         /* upgrading users */
         if (prefsVersionCode > 0) {
@@ -74,5 +51,55 @@ public class MigrationManager {
         } else /* new users */ {
             ABManager.getInstance().assignNewUsers();
         }
+
+        prefs.edit().putInt(PREFS_VERSION_CODE, version).apply();
+    }
+
+    /**
+     * Let Search handle search language
+     */
+    private void migrateBackendCountryLanguage() {
+        PrefsHelper.getPref(GeckoPreferences.PREFS_SEARCH_REGIONAL, new PrefsHelper.PrefHandlerBase() {
+            @Override
+            public void prefValue(String prefName, String countryCode) {
+                if (countryCode == null || countryCode.equals("")) {
+                    return;
+                }
+                PrefsHelper.setPref(GeckoPreferences.PREFS_SEARCH_REGIONAL, "");
+                SearchBackground.setBackendCountry(countryCode);
+            }
+        });
+    }
+
+    private void migrateQuerySuggestionsPref() {
+        PrefsHelper.getPref(GeckoPreferences.PREFS_SEARCH_QUERY_SUGGESTIONS, new PrefsHelper.PrefHandlerBase() {
+            @Override
+            public void prefValue(String prefName, boolean enabled) {
+                PrefsHelper.setPref(GeckoPreferences.PREFS_SEARCH_QUERY_SUGGESTIONS, "");
+                PrefsHelper.setPref(PrefsModule.PREFS_SEARCH_QUERY_SUGGESTIONS, enabled);
+            }
+        });
+    }
+
+    private int getPreviousVersionCode(SharedPreferences prefs) {
+        int versionCode = prefs.getInt(PREFS_VERSION_CODE, 0);
+
+        // migrate the version code from gecko
+        if (versionCode == 0) {
+            versionCode = prefs.getInt(GeckoApp.PREFS_VERSION_CODE, 0);
+            prefs.edit().putInt(PREFS_VERSION_CODE, versionCode).apply();
+        }
+
+        return versionCode;
+    }
+
+    private int getVersionCode(Context context) {
+        int versionCode = 0;
+        try {
+            versionCode = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.wtf(getClass().getSimpleName(), context.getPackageName() + " not found", e);
+        }
+        return versionCode;
     }
 }
